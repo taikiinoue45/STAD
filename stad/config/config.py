c07cb792d66dfc61ce09d395b42b911d6fd2ed2a
TaikiInoue <taikiinoue45@gmail.com>
2020-06-27 20:37:55 +0900

    Initial commit

--- stad/config/config.py
+++
@@ -1,8 +0,0 @@
-import argparse
-import yaml
-
-parser = argparse.ArgumentParser()
-parser.add_argument('yaml', help='yaml path', type=str)
-args = parser.parse_args()
-with open(args.yaml) as f:
-    Y = yaml.safe_load(f)
