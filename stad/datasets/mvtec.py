199c1e0c062c4daa4b1a6b5844af1d7365a71105
TaikiInoue <taikiinoue45@gmail.com>
2020-09-14 11:23:42 +0000

    Change the data structure

--- stad/datasets/mvtec.py
+++ stad/datasets/mvtec.py
@@ -1,15 +1,14 @@
-import cv2
-import numpy as np
-import albumentations as albu
-
 from pathlib import Path
+
+import albumentations as albu
+import cv2
 from torch.utils.data import Dataset
 
 
 class MVTecDataset(Dataset):
     def __init__(self, base: Path, augs: albu.Compose) -> None:
 
-        self.img_paths = [str(p) for p in base.glob("images/*.png")]
+        self.img_paths = [str(p) for p in base.glob("*.png")]
         self.augs = augs
 
     def __getitem__(self, idx: int):
