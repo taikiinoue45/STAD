1986f592340234b598eb41ed068891cea5f3ead7
TaikiInoue <taikiinoue45@gmail.com>
2021-01-05 09:42:46 +0000

    Add experiments for all categories in MVTec

--- stad/run.py
+++ stad/run.py
@@ -8,9 +8,6 @@ from omegaconf import DictConfig
 from stad.runner import Runner
 
 
-mlflow.set_tracking_uri("databricks")
-mlflow.set_experiment("/Users/inoue@nablas.com/stad")
-
 config_path = sys.argv[1]
 sys.argv.pop(1)
 
@@ -18,7 +15,12 @@ sys.argv.pop(1)
 @hydra.main(config_path)
 def main(cfg: DictConfig) -> None:
 
+    mlflow.set_tracking_uri("databricks")
+    mlflow.set_experiment("/Users/inoue@nablas.com/stad")
+    mlflow.start_run(run_name=cfg.alias.category)
+
     os.rename(".hydra", "hydra")
+    mlflow.log_artifacts("hydra")
 
     runner = Runner(cfg)
     runner.run()
